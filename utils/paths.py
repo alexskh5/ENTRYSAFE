6096845831111905ec380abdf42c1d68929e0e0b
Sam Alexies H. Dela Peña <samalexies.delapena@ctu.edu.ph>
2026-02-19 19:21:50 +0800

    Add resource_path function for file handling
    Added helper function to construct resource paths.

--- utils/paths.py
+++ utils/paths.py
@@ -2,8 +2,25 @@ import os
 import sys
 
 def app_dir():
+    """
+    Returns the root folder of the app.
+    Works for:
+    - python main.py
+    - PyInstaller onedir exe
+    """
+
     # If running as EXE (PyInstaller)
     if getattr(sys, "frozen", False):
         return os.path.dirname(sys.executable)
+
     # Normal python run
     return os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+
+def resource_path(*paths):
+    """
+    Helper to safely get files like:
+    ui files, assets, icons, uploads, etc.
+    """
+
+    return os.path.join(app_dir(), *paths)
